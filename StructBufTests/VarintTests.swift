f8cb4eb8fca135714da3f6104b7d44650834ec8e
David Paschich <davidp@navdy.com>
2015-06-11 14:21:28 -0700

    Copy over our code so far from the other repo.

---
+++ StructBufTests/VarintTests.swift
@@ -0,0 +1,110 @@
+//
+//  VarintTests.swift
+//  ProtocolBuffers
+//
+//  Created by David Paschich on 6/9/15.
+//  Copyright © 2015 David Paschich. All rights reserved.
+//
+
+import XCTest
+import StructBuf
+
+class VarintTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testInitUInt64() {
+        let number: UInt64 = 0
+        let varint = Varint(value: number)
+
+        XCTAssert(varint.bytes.count > 0)
+    }
+
+    func testInitUInt64LargeNumber() {
+        let number: UInt64 = 10121342435
+        let varint = Varint(value: number)
+
+        XCTAssert(varint.bytes.count > 0)
+    }
+
+    func testInitUInt32() {
+        let number: UInt32 = 42
+        let varint = Varint(value: number)
+
+        XCTAssertGreaterThan(varint.bytes.count, 0)
+    }
+
+    func testInitInt() throws {
+        let number: Int = 1
+        let varint = try Varint(value: number)
+
+        XCTAssertGreaterThan(varint.bytes.count, 0)
+    }
+
+    func testInitIntNegativeNumber() {
+        let number: Int = -4
+        do {
+            _ = try Varint(value: number)
+        } catch VarintError.NotImplemented {
+            return // test passes
+        } catch {
+            XCTFail("threw wrong error")
+        }
+        XCTFail("should have thrown error")
+    }
+
+    func testTooShortVarintThrows() {
+        let inputBytes: [UInt8] = [0x80]
+
+        do {
+            _ = try Varint.fromBytes(inputBytes)
+        } catch {
+            // test passed, return
+            return
+        }
+        XCTFail("parse should have vailed")
+    }
+
+    func testSingleByteInt() {
+        let inputBytes: [UInt8] = [0x01]
+
+        do {
+            let (_, bytesConsumed) = try Varint.fromBytes(inputBytes)
+            XCTAssertEqual(bytesConsumed, 1)
+
+        } catch {
+            XCTFail("should not have thrown")
+        }
+
+    }
+
+    func testTooManyBytes() {
+        let inputBytes = Array<UInt8>(count: 11, repeatedValue: 0x80)
+
+        do {
+            _ = try Varint.fromBytes(inputBytes)
+        } catch {
+            // test passed, return
+            return
+        }
+        XCTFail("parse should have vailed")
+    }
+
+    func testAsBool() throws {
+        let varint = try Varint(value: 40)
+        XCTAssertTrue(varint.as_bool())
+    }
+
+    func testAsBoolNoBytes() throws {
+        let varint = Varint(bytes: [])
+        XCTAssertFalse(varint.as_bool())
+    }
+}
---
+++ protoc-swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  protoc-swift
+//
+//  Created by David Paschich on 6/11/15.
+//  Copyright © 2015 David Paschich. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
