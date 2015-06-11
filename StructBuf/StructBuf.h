7aa942e89bb99d7fe1f30dd5b0ab71a46a6fcec8
David Paschich <davidp@navdy.com>
2015-06-11 13:27:57 -0700

    New project infrastructure to turn this into something real.

---
+++ StructBuf/StructBuf.h
@@ -0,0 +1,19 @@
+//
+//  StructBuf.h
+//  StructBuf
+//
+//  Created by David Paschich on 6/11/15.
+//  Copyright © 2015 David Paschich. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for StructBuf.
+FOUNDATION_EXPORT double StructBufVersionNumber;
+
+//! Project version string for StructBuf.
+FOUNDATION_EXPORT const unsigned char StructBufVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StructBuf/PublicHeader.h>
+
+
