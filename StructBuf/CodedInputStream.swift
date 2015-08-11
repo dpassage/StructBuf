c3f9e8127435f5919358beb1606cc13d9ca9e68e
David Paschich <dpassage@balveda.com>
2015-08-10 21:56:30 -0700

    some fixed32 parsing

--- StructBuf/CodedInputStream.swift
+++ StructBuf/CodedInputStream.swift
@@ -10,12 +10,22 @@ import Cocoa
 
 class CodedInputStream {
 
-    init(bytes: [UInt8]) {
+    var bytes: [UInt8]
 
+    init(bytes: [UInt8]) {
+        self.bytes = bytes
     }
 
-    func readFixed32Unsigned() -> UInt32 {
-        return 0
+    func readFixed32Unsigned() throws -> UInt32 {
+        guard bytes.count >= 4 else { throw StructBufError.ParseFailed }
+        var result: UInt32 = 0
+        result += UInt32(bytes[0])
+        result += UInt32(bytes[1]) << 8
+        result += UInt32(bytes[2]) << 16
+        result += UInt32(bytes[3]) << 24
+
+        bytes = [UInt8](bytes.dropFirst(4))
+        return result
     }
 
     func generator() throws -> AnyGenerator<Field> {
--- StructBufTests/CodedInputStreamSpec.swift
+++ StructBufTests/CodedInputStreamSpec.swift
@@ -14,19 +14,48 @@ import Nimble
 class CodedInputStreamSpec: QuickSpec {
 
     override func spec() {
-        it("can yield a fixed32") {
-            let stream = CodedInputStream(bytes: [0x0,0x0,0x0,0x0])
-            let result = stream.readFixed32Unsigned()
-            expect(result).to(equal(0))
-        }
+        describe("Fixed32") {
+            it("can yield a fixed32") {
+                let stream = CodedInputStream(bytes: [0x0,0x0,0x0,0x0])
+                let result = try! stream.readFixed32Unsigned()
+                expect(result).to(equal(0))
+            }
 
-        it("returns a generator") {
-            do {
-                let stream = CodedInputStream(bytes: [])
-                _ = try stream.generator()
-            } catch {
-                XCTFail("should not have thrown")
+            it("can correctly yield a random number") {
+                for _ in 0...1 {
+                    let number = arc4random()
+                    let bytes = [UInt8(number & 0xff),
+                                 UInt8((number >> 8) & 0xff),
+                                 UInt8((number >> 16) & 0xff),
+                                 UInt8((number >> 24) & 0xff)]
+                    let stream = CodedInputStream(bytes: bytes)
+                    let result = try! stream.readFixed32Unsigned()
+                    expect(result).to(equal(number))
+                }
+            }
+
+            it("throws when not enough bytes") {
+                let bytes: [UInt8] = [0x11, 0x37]
+                let stream = CodedInputStream(bytes: bytes)
+                do {
+                    try _ = stream.readFixed32Unsigned()
+                } catch StructBufError.ParseFailed {
+                    // passed
+                    return
+                } catch {
+                    XCTFail("threw wrong error")
+                }
+                XCTFail("should have thrown")
             }
         }
+
+//        it("returns a generator") {
+//            do {
+//                let stream = CodedInputStream(bytes: [])
+//                let _ = try stream.generator()
+//            } catch {
+//                XCTFail("should not have thrown")
+//            }
+//        }
     }
 }
